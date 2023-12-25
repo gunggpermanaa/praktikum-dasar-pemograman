92b0a2555f1ebe1ed4ccc6536d643e20048c39ea
agung permana <144643400+gunggpermanaa@users.noreply.github.com>
2023-12-25 12:48:47 +0700

    Delete main.java

--- main.java
+++
@@ -1,8 +0,0 @@
-public class main {
-    public static void main(String[] args) {
-        Window window = Window.getWindow();
-
-        Thread thread = new Thread(window);
-        thread.start();
-    }
-}
