a05a5f7c2a3b8fa87b755abc8292013a46966da8
agung permana <144643400+gunggpermanaa@users.noreply.github.com>
2023-12-25 12:48:09 +0700

    Delete Constants.java

--- Constants.java
+++
@@ -1,7 +0,0 @@
-public class Constants {
-    public static final int SCREEN_WIDTH = 800;
-    public static final int SCREEN_HEIGHT = 600;
-    public static final String SCREEN_TITLE = "Snake";
-
-    public static final float TILE_WIDTH = 24;
-}
